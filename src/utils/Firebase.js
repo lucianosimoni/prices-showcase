85930a3569cffadd99550213358e1c047be5afc0
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-01-04 19:04:59 +0000

    firebase added

---
+++ src/utils/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmL00GjK4ITdZCSA3JouBuLy5saQzI6NQ",
+  authDomain: "prices-showcase.firebaseapp.com",
+  databaseURL:
+    "https://prices-showcase-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "prices-showcase",
+  storageBucket: "prices-showcase.appspot.com",
+  messagingSenderId: "343227628906",
+  appId: "1:343227628906:web:40961511c79670a07d3913",
+  measurementId: "G-5KB64C8T33",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
